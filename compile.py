554228fca7f23bf4d33a598d1a09807f002c56f6
Dylan Corrales <DeathCamel58@gmail.com>
2023-10-21 21:33:57 -0400

    Initial Commit

---
+++ compile.py
@@ -0,0 +1,64 @@
+import os
+
+from systemrdl import *
+from peakrdl_html import HTMLExporter
+
+roots = []
+
+
+def get_rdl_folders():
+    """
+    Gets all of the RDL chip folders
+    :return: The list chip names
+    """
+    folders = os.listdir("rdl")
+
+    print("Found RDL folders:")
+    if len(folders) == 0:
+        print("\tNo RDL folders found!")
+        exit(1)
+    else:
+        for folder in folders:
+            print("\t%s" % folder)
+
+    return folders
+
+
+def process_chip(chip):
+    """
+    Compiles each chip, and appends them to the `roots` array
+    :param chip: The chip folder name
+    :return: None
+    """
+    print("Processing RDL file at rdl/%s/%s.rdl" % (chip, chip))
+
+    # Include all the RDL files in the chip's directory
+    rdl_files = os.listdir("rdl/%s" % chip)
+    for i in range(len(rdl_files) - 1, -1, -1):
+        # Fix the filename of the RDL files, and remove the base RDL file from the array
+        if rdl_files[i] != "%s.rdl" % chip:
+            rdl_files[i] = "rdl/%s/%s" % (chip, rdl_files[i])
+        else:
+            rdl_files.pop(i)
+
+    rdl = RDLCompiler()
+
+    try:
+        rdl.compile_file("rdl/%s/%s.rdl" % (chip, chip), rdl_files)
+
+        root = rdl.elaborate()
+        roots.append(root)
+    except RDLCompileError as e:
+        print(e)
+        exit(1)
+
+
+if __name__ == '__main__':
+    folders = get_rdl_folders()
+
+    for folder in folders:
+        process_chip(folder)
+
+    # Generate the documentation site
+    exporter = HTMLExporter()
+    exporter.export(roots, "out/web/", title="Hisilicon Registers")
